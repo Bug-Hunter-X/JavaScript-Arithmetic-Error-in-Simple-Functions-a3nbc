3e4f74abbfe7225d38e95f14f232c732d2085593
BugHunter-X <askzenai@gmail.com>
2025-01-23 04:54:04 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,10 @@
+function foo(a, b) {
+  return a + b; //Corrected
+}
+
+function bar(a, b) {
+  return a - b; //Corrected
+}
+
+console.log(foo(1, 2)); // Output: 3
+console.log(bar(1, 2)); // Output: -1
